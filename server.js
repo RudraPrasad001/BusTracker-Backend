bad1a8e84bb8d145c2bfd823fd5adf93c4af77e6
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-07-15 12:33:26 +0530

    SQL Initialization

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import initializeDB from "./utils/initializeDB.js";
 const app = express();
+initializeDB();
 app.listen(3000,()=>{
     console.log("Server Running");
 })
