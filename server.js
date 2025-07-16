c0417b1b16adbcd42e0960f50e56c6143bc039fd
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-07-16 14:50:43 +0530

    Driver OTP starting

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ import initializeDB from "./utils/initializeDB.js";
 import adminRouter from "./routes/admin_routes/adminRoutes.js";
 import bodyParser from "body-parser";
 import basicRouter from "./routes/basic_routes/basicRoutes.js";
+import driverRouter from "./routes/driver_routes/driverRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 //To initialize the tables
 initializeDB();
 app.use(express.json());
 app.use("/admin",adminRouter);
+app.use("/driver",driverRouter);
 app.use("/",basicRouter);
 app.listen(3000,()=>{
     console.log("Server Running");
