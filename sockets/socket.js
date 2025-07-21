c26e9c95ea8a501d95ab6b0210f4ab8dd07ce8cc
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-07-21 21:21:42 +0530

    Map Integration Done

--- sockets/socket.js
+++ sockets/socket.js
@@ -12,23 +12,24 @@ const initializeSocket = (server) => {
 
   const busTrackers = {};
 
-  io.on("connection", (socket) => {
-    console.log("🚐 A client connected:", socket.id);
+io.on("connection", (socket) => {
+  console.log("🚐 A client connected:", socket.id);
 
-    socket.on("start-tracking", (busId) => {
-      socket.join(busId);
-      console.log(`👀 Client ${socket.id} is tracking bus ${busId.bus_number}`);
-    });
-
-    socket.on("location-update", ({ busId, coords }) => {
-      console.log(`📍 Location for,` , busId.bus_number,`:`, coords);
-      io.to(busId).emit("bus-location", coords);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("❌ Client disconnected:", socket.id);
-    });
+  socket.on("start-tracking", ({ BUS_ID }) => {
+    socket.join(BUS_ID);
+    console.log(`👀 Client ${socket.id} is tracking bus ${BUS_ID}`);
   });
+
+  socket.on("location-update", ({ busId, coords }) => {
+    console.log(`📍 Location for bus ${busId}:`, coords);
+    io.to(busId).emit("bus-location", coords);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Client disconnected:", socket.id);
+  });
+});
+
 };
 
 export default  initializeSocket ;
