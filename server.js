e59360aa695652c909e1aaf70b82fa28ce0ab5ff
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-07-21 11:41:41 +0530

    Send Driver Location no WEBSOCKET

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import cors from "cors";
+import http from "http";
 import initializeDB from "./utils/initializeDB.js";
 import adminRouter from "./routes/admin_routes/adminRoutes.js";
 import bodyParser from "body-parser";
 import basicRouter from "./routes/basic_routes/basicRoutes.js";
 import driverRouter from "./routes/driver_routes/driverRoutes.js";
+import initializeSocket from "./sockets/socket.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
 //To initialize the tables
 initializeDB();
+initializeSocket(server);
 app.use(express.json());
 app.use(cors({origin:"*"}));
 app.use("/admin",adminRouter);
 app.use("/driver",driverRouter);
 app.use("/",basicRouter);
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log("Server Running");
 })
---
+++ sockets/socket.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+
+let io;
+
+const initializeSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  const busTrackers = {};
+
+  io.on("connection", (socket) => {
+    console.log("🚐 A client connected:", socket.id);
+
+    socket.on("start-tracking", (busId) => {
+      socket.join(busId);
+      console.log(`👀 Client ${socket.id} is tracking bus ${busId}`);
+    });
+
+    socket.on("location-update", ({ busId, coords }) => {
+      console.log(`📍 Location for ${busId}:`, coords);
+      io.to(busId).emit("bus-location", coords);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("❌ Client disconnected:", socket.id);
+    });
+  });
+};
+
+export default  initializeSocket ;
